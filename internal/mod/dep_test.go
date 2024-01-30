693ef4f1ac16b82e07ec9d6fc44a450cb109f8e3
Aryan Sjet <sjet@asjet.dev>
2024-01-30 22:54:40 +0800

    feat(internal/mod): add dependency resolve

---
+++ internal/mod/dep_test.go
@@ -0,0 +1,58 @@
+package mod
+
+import (
+	"testing"
+)
+
+func TestDepTree(t *testing.T) {
+	deps := []*DepNode{
+		Dep(1, 4),
+		Dep(2, 4),
+		Dep(3, 1),
+		Dep(3, 2),
+		Dep(4, 7),
+		Dep(4, 8),
+		Dep(5, 1),
+		Dep(5, 4),
+		Dep(5, 6),
+		Dep(6, 10),
+		Dep(6, 11),
+		Dep(7, 9),
+		Dep(8, 9),
+		Dep(8, 10),
+		Dep(9, 12),
+		Dep(10, 12),
+		Dep(10, 13),
+		Dep(11, 10),
+		Dep(6, 10),
+		Dep(6, 11),
+		Dep(7, 9),
+		Dep(8, 9),
+		Dep(8, 10),
+		Dep(9, 12),
+		Dep(10, 12),
+		Dep(10, 13),
+		Dep(11, 10),
+	}
+	depMap := make(map[int]map[int]bool)
+	for _, dep := range deps {
+		if _, ok := depMap[dep.Node]; !ok {
+			depMap[dep.Node] = make(map[int]bool)
+		}
+		for _, d := range dep.Deps {
+			depMap[dep.Node][d] = true
+		}
+	}
+
+	ts := NewDepTree(deps...).TopSort()
+	exist := make(map[int]bool)
+	for _, node := range ts {
+		for dep := range depMap[node] {
+			if !exist[dep] {
+				t.Errorf("Node %d expect dep %d exist but not", node, dep)
+			}
+		}
+		exist[node] = true
+	}
+
+}

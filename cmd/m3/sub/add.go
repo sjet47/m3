32cc9759984cd5e70a17e3f73c2bd8c36200fdc7
Aryan Sjet <sjet@asjet.dev>
2024-02-06 16:27:40 +0800

    feat(sub/add): read mod ids from file

--- cmd/m3/sub/add.go
+++ cmd/m3/sub/add.go
@@ -1,20 +1,24 @@
 package sub
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"slices"
 	"strconv"
+	"strings"
 
 	"github.com/ASjet/m3/internal/index"
 	"github.com/ASjet/m3/internal/mod"
 	"github.com/ASjet/m3/internal/util"
-
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 var (
 	optDep    bool
 	modLoader string
+	fileInput string
 )
 
 func init() {
@@ -22,14 +26,33 @@ func init() {
 		"optional", "o", false, "Download optional dependencies")
 	subCmdAdd.Flags().StringVarP(&modLoader,
 		"modloader", "l", "Forge", "Mod loader")
+	subCmdAdd.Flags().StringVarP(&fileInput,
+		"file", "f", "", "Read mod ids from csv file")
 	rootCmd.AddCommand(subCmdAdd)
 }
 
 var subCmdAdd = &cobra.Command{
 	Use:   "add <mod_id>...",
 	Short: "Add mods to the m3 index",
-	Args:  cobra.MinimumNArgs(1),
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(fileInput) > 0 {
+			return cobra.ExactArgs(0)(cmd, args)
+		}
+		return cobra.MinimumNArgs(1)(cmd, args)
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(fileInput) > 0 {
+			f, err := os.Open(fileInput)
+			if err != nil {
+				return errors.Wrapf(err, "open file %s error", fileInput)
+			}
+			defer f.Close()
+			scanner := bufio.NewScanner(f)
+			for scanner.Scan() {
+				args = append(args, strings.Split(scanner.Text(), ",")[0])
+			}
+		}
+
 		modIDs, errIndex, err := util.MapErr(strconv.Atoi, args...)
 		if err != nil {
 			return fmt.Errorf("invalid mod id %q", args[errIndex])
